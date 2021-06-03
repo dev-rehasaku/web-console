827eb2b4f380910f4437fa890ae1d3b188ec9f00
cornpotage <potage.recipe@gmail.com>
2021-06-03 15:21:56 +0900

    REHASAKU-86 mod: change source location code on ExceptionMapper.

--- lib/web_console/exception_mapper.rb
+++ lib/web_console/exception_mapper.rb
@@ -2,9 +2,28 @@
 
 module WebConsole
   class ExceptionMapper
+    attr_reader :exc
+
+    def self.follow(exc)
+      mappers = [new(exc)]
+
+      while cause = (cause || exc).cause
+        mappers << new(cause)
+      end
+
+      mappers
+    end
+
+    def self.find_binding(mappers, exception_object_id)
+      mappers.detect do |exception_mapper|
+        exception_mapper.exc.object_id == exception_object_id.to_i
+      end || mappers.first
+    end
+
     def initialize(exception)
       @backtrace = exception.backtrace
       @bindings = exception.bindings
+      @exc = exception
     end
 
     def first
@@ -17,19 +36,21 @@ module WebConsole
 
     private
 
-      def guess_binding_for_index(index)
-        file, line = @backtrace[index].to_s.split(":")
-        line = line.to_i
+    def guess_binding_for_index(index)
+      file, line = @backtrace[index].to_s.split(":")
+      line = line.to_i
 
-        @bindings.find do |binding|
-          binding.eval("__FILE__") == file && binding.eval("__LINE__") == line
-        end
+      @bindings.find do |binding|
+        source_location = SourceLocation.new(binding)
+        source_location.path == file && source_location.lineno == line
       end
+    end
 
-      def guess_the_first_application_binding
-        @bindings.find do |binding|
-          binding.eval("__FILE__").to_s.start_with?(Rails.root.to_s)
-        end
+    def guess_the_first_application_binding
+      @bindings.find do |binding|
+        source_location = SourceLocation.new(binding)
+        source_location.path.to_s.start_with?(Rails.root.to_s)
       end
+    end
   end
 end
